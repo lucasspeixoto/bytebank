c0a20b41e244119fe1e2f9f885c0ed614fe6f9d8
Lucas Peixoto <lspeixotodev@gmail.com>
2022-02-16 23:44:50 -0300

    Finalização layout da tela de contatos

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,53 +30,6 @@ class BytebankApp extends StatelessWidget {
         ),
       ),
       home: const Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Image.asset('assets/images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              width: 150,
-              height: 100,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/contact_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  const ContactsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contacts"),
+      ),
+      body: ListView(
+        children: const <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('Lucas', style: TextStyle(fontSize: 24)),
+              subtitle: Text('1000', style: TextStyle(fontSize: 16)),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
